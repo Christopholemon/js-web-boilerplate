0a152522aff2e31bb661810b0a27da0f0e6e65ce
Christopholemon <chris.a.klim@gmail.com>
2020-08-08 00:46:53 +1000

    Updated paths and server requests

--- server.js
+++ server.js
@@ -12,10 +12,20 @@ app
   .use(
     helmet(),
     cors(corsOptions),
+    express.static('dist')
   )
-  .use('/', express.static('./dist', {
-    index: "index.html"
-  }))
+  
+  .get('/',function(req,res) {
+    console.log(">> GET", path.join(__dirname+'/dist/index.html'));
+
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+
+    // console.log(">> GET REQUEST", `${path.join(__dirname, 'dist')}/index.html`);
+
+    // res.sendFile('/index.html', { 
+    //   root: path.join(__dirname, 'dist')
+    // });
+  })
   .listen(port, () => console.log(`>> App listening on port: ${port}`));
 
 // Node
