5d7db00628d5c0c8ea3d609f83a37807da9d7209
Christopholemon <chris.a.klim@gmail.com>
2020-08-07 21:42:40 +1000

    Updating and stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import path from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   watch: false,
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/scripts/index.js',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
