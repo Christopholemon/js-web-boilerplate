ba6bf0668954794775c0e7c4a88a9c300feb7f8e
Christopholemon <chris.a.klim@gmail.com>
2020-08-12 20:20:46 +1000

    working locally... for now

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 import express from 'express';
 import path from 'path';
 // import cors from 'cors';
@@ -11,7 +10,7 @@ const port = process.env.PORT;
 app
   // .use(cors(corsOptions))
   // .use(helmet())
-  .use(express.static(path.join(__dirname, 'dist')))
+  // .use(express.static(path.join(__dirname, 'dist')))
   .get('/',function(req,res) {
     // res.sendFile('/dist/index.html');
     res.send('???')
@@ -27,15 +26,3 @@ app
 // Babel
 // React + react dom
 // React hooks or separate state management?
-*/
-
-import express from 'express';
-
-const app = express();
-const PORT = 3333;
-
-app
-  .get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  .listen(PORT, () => console.log(`>> RUNNING AND LISTENING ON PORT ${PORT}`))
