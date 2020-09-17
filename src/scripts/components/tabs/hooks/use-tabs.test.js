214ab2f405f34c2b2f349eacc589ea5eeab4e351
Christopholemon <chris.a.klim@gmail.com>
2020-09-17 11:05:58 +1000

    Adding new test libraries, adding references to look at for testing actual click events

--- src/scripts/components/tabs/hooks/use-tabs.test.js
+++ src/scripts/components/tabs/hooks/use-tabs.test.js
@@ -9,6 +9,7 @@ describe('hook: useTabs', () => {
       expect(result.current.currentTab).toBe(0);
     });
 
+    /*
     test('event: handleClick', () => {
       const { result } = renderHook(() => useTabs.useTabController(0));
 
@@ -25,6 +26,38 @@ describe('hook: useTabs', () => {
       expect(result.current.currentTab).toBe(2);
 
     });
+    */
 
+    /*
+      import React from "react";
+      import { render, fireEvent } from "@testing-library/react";
+      import Todo from "../Todo";
+      import "@testing-library/jest-dom/extend-expect";
+
+      test("Todo", () => {
+        // Tests go here
+        it("displays initial to-dos", () => {
+          const { getByTestId } = render(<Todo />);
+          const todos = getByTestId("todos");
+          expect(todos.children.length).toBe(2);
+        });
+        it("adds a new to-do", () => {
+          const { getByTestId, getByText } = render(<Todo />);
+          const input = getByTestId("input");
+          const todos = getByTestId("todos");
+          input.value = "Fix failing tests";
+          fireEvent.click(getByText("Add Task"));
+          expect(todos.children.length).toBe(3);
+        });
+        it("deletes a to-do", () => {
+          const { getAllByTestId, getByTestId } = render(<Todo />);
+          const todos = getByTestId("todos");
+          const deleteButton = getAllByTestId("delete-button");
+          const first = deleteButton[0];
+          fireEvent.click(first);
+          expect(todos.children.length).toBe(1);
+        });
+      }
+    */
   })
 });
